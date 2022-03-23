b9c18ac1130901bc69eefe5ebf32899ec96d506e
TAM <102209765+TAMvip@users.noreply.github.com>
2022-03-23 17:42:56 +0800

    Add files via upload

---
+++ static/js/lang.js
@@ -0,0 +1,11 @@
+import common from '../../common/commonLang.js'
+export default {	
+	us: {
+		common: common.us,
+		langList : ['英文版', '中文版']
+	},
+	cn: {
+		common: common.cn,
+		langList : ['英文版', '中文版']
+	},
+}
